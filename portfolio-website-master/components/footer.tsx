b45d8216387eb3fc32c8fcbc385e8631d269f39c
asliabhi12 <asliabhi12@gmail.com>
2024-02-16 01:15:59 +0530

    done

--- portfolio-website-master/components/footer.tsx
+++ portfolio-website-master/components/footer.tsx
@@ -7,7 +7,8 @@ export default function Footer() {
         &copy; 2024 Abhishek Rajput. All rights reserved.
       </small>
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
+        <span className="fo
+        nt-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
         Framer Motion, React Email & Resend, Vercel hosting.
       </p>
